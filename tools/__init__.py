59193d4348c3f26ed820a033e23e32207cf24b98
Bettina <bettina.l.hein@gmail.com>
2021-04-11 13:21:51 -0400

    add userfriendly image path

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,6 @@
 import os
 
-# base_path = os.environ["HOMe"] + "pattern_formation/image/"
-base_path = "/home/bettina/physics/fias/projects/pattern_formation/publish/"
+base_path = os.environ["HOME"] + "physics/fias/projects/pattern_formation/publish/"
 image_dir = base_path + "image/"
 if not os.path.exists(image_dir):
 	os.makedirs(image_dir)
